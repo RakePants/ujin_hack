b8cda3f8338b04dfe69707e8b4bb7b256098e3e1
Artyom Eryomkin <eryomkin.artyom2016@gmail.com>
2024-05-31 04:16:09 +0500

    Integrate passes

--- app/src/infrastructure/integrations/omissions/base.py
+++ app/src/infrastructure/integrations/omissions/base.py
@@ -5,7 +5,3 @@ class BaseOmissions(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     async def create_omission(self, omission):
         raise NotImplementedError
-
-    @abc.abstractmethod
-    async def get_omission(self, omissions_id):
-        raise NotImplementedError
