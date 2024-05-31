c059e11ab33216811bf3e60b923e6de960a4ea2a
Maxim Osetrov <maksimosetrov4@gmail.com>
2024-05-31 10:50:05 +0500

    fix docker-compose

--- app/src/config/config.py
+++ app/src/config/config.py
@@ -4,6 +4,6 @@ from pydantic import Field
 
 class Config(BaseSettings):
     #model_config = SettingsConfigDict(env_file='../../.env')
-    MONGODB_CONNECTION_URI: str = Field(alias='MONGODB_CONNECTION_URI', default='mongodb://localhost:27017/')
+    MONGODB_CONNECTION_URI: str = Field(alias='MONGODB_CONNECTION_URI', default='mongodb://mongodb-service:27017/')
     UJIN_CON_TOKEN: str = Field(alias='UJIN_CON_TOKEN', default='ust-739109-3fe72efd12ef86582919741571b1cb40')
     UJIN_HOST: str = Field(alias='UJIN_HOST', default='api-uae-test.ujin.tech')
